c78dcaa51e51b97df2b358620306000187e6eee5
Malgorzata <jarzemska.malgorzata@gmail.com>
2019-02-23 16:55:23 +0100

    Upload

--- js/app.jsx
+++ js/app.jsx
@@ -10,24 +10,11 @@ import {
 import AddFlat from './components/addflat.jsx';
 import FlatsListTable from './components/list.jsx'
 
-
-class FlatsList extends React.Component {
-    render() {
-        return (
-            <div>
-                <NavLink to="/">Strona główna</NavLink>
-                <h2>Lista nieruchomości</h2>
-                <FlatsListTable />
-            </div>
-        )
-    }
-}
-
 class NewFlat extends React.Component {
     render() {
         return (
-            <div>
-                <NavLink to="/">Strona główna</NavLink>
+            <div className="container">
+                <NavLink to="/" className="backTo">Strona główna</NavLink>
                 <h2>Nowa nieruchomość</h2>
                 <AddFlat />
             </div>
@@ -36,27 +23,74 @@ class NewFlat extends React.Component {
 }
 
 
+class FlatsList extends React.Component {
+    render() {
+        return (
+            <div className="container">
+                <NavLink to="/" className="backTo">Strona główna</NavLink>
+                <h2>Lista nieruchomości</h2>
+                <FlatsListTable />
+            </div>
+        )
+    }
+}
+
+
+
+class Monitoring extends React.Component {
+    render() {
+        return (
+            <div className="container">
+                <NavLink to="/" className="backTo">Strona główna</NavLink>
+                <h2>Monitoruj zyski i koszty</h2>
+            </div>
+        )
+    }
+}
+
 class Main extends React.Component {
     render() {
         return (
-            <div>
-                <NavLink to="/newflat">Dodaj nieruchomość</NavLink>
-                <NavLink to="/flatslist">Zobacz listę nieruchomości</NavLink>
+            <div id="navigation">
+                <NavLink to="/newflat" className="nav">Dodaj nieruchomość</NavLink>
+                <NavLink to="/flatslist" className="nav">Zobacz listę nieruchomości</NavLink>
+                <NavLink to="/monitoring" className="nav">Monitoruj zyski i koszty</NavLink>
             </div>
         )
     }
 }
 
 class App extends React.Component {
+
+    // fetchDiki = () => {
+    //     const url = 'http://localhost:3000/dictionaries';
+    //     fetch(url)
+    //         .then(resp => resp.json())
+    //         .then(resp => {
+    //             this.setState({
+    //                 dictionaries: resp[0],
+    //             })
+    //             console.log(resp);
+    //             console.log(this.state);
+    //         }).catch(err => {
+    //             console.log(err);
+    //         })
+    // }
+
+    // componentDidMount() {
+    //     setTimeout(this.fetchDiki, 1)
+    // }
+
     render() {
         return (
             <HashRouter>
                 <div>
-                    <h1>Zarządzaj nieruchomościami</h1>
+                    <h1 id="appName">Dev-App</h1>
                     <Switch>
                         <Route exact path='/' component={Main} />
                         <Route path='/newflat' component={NewFlat} />
                         <Route path='/flatslist' component={FlatsList} />
+                        <Route path="/monitoring" component={Monitoring} />
                     </Switch>
                 </div>
             </HashRouter>
--- js/components/list.jsx
+++ js/components/list.jsx
@@ -1,5 +1,7 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import ElementList from './list_elements.jsx';
+import ContentEditable from 'react-contenteditable';
 import {
     HashRouter,
     Route,
@@ -8,46 +10,63 @@ import {
     NavLink,
 } from 'react-router-dom';
 
-
 class FlatsListTable extends React.Component {
+    state = {
+        info: [],
+        dictionaries: [],
+        isLoaded: false,
+        contentEditable: false,
+    }
+
+    fetchData = () => {
+        const url = 'http://localhost:3000/info';
+        fetch(url)
+            .then(resp => resp.json())
+            .then(resp => {
+                this.setState({
+                    info: resp,
+                    isLoaded: true,
+                })
+            }).catch(err => {
+                console.log(err)
+            })
+    }
+
+    fetchDiki = () => {
+        const url = 'http://localhost:3000/dictionaries';
+        fetch(url)
+            .then(resp => resp.json())
+            .then(resp => {
+                this.setState({
+                    dictionaries: resp[0],
+                });
+                console.log(this.state)
+            }).catch(err => {
+                console.log(err);
+            })
+    }
+
+
+    componentDidMount() {
+        setTimeout(this.fetchData, 1000);
+        setTimeout(this.fetchDiki, 1);
+        console.log(this.state);
+    }
+
     render() {
+        let dic = this.state.dictionaries.prop;
+        console.log(dic);
+        const list = this.state.info.map(info => (
+            <ElementList key={info.number} type={info.number} address={info.property.address.street} city={info.property.address.city} />
+        ));
+        const edBtn = <button contentEditable={this.state.contentEditable}>Edytuj</button>;
+        const rmBtn = <button >Usuń</button>;
         return (
-            <div>
-                <section>
-                    <h3>Lista wynajmowanych mieszkań:</h3>
-                    <ul>
-                        <li></li>
-                    </ul>
-                </section>
-                <section>
-                    <h3>Lista wynajmowanych domów:</h3>
-                    <ul>
-                        <li></li>
-                    </ul>
-                </section>
-
-                <section>
-                    <h3>Lista wynajmowanych pokoi:</h3>
-                    <ul>
-                        <li></li>
-                    </ul>
-                </section>
-
-                <section>
-                    <h3>Lista wynajmowanych apartamentów:</h3>
-                    <ul>
-                        <li></li>
-                    </ul>
-                </section>
-
-                <section>
-                    <h3>Lista wynajmowanych garaży:</h3>
-                    <ul>
-                        <li></li>
-                    </ul>
-                </section>
-
-            </div>
+            <ul>
+                {this.state.isLoaded ? list : "Wczytuję dane..."}
+                {this.state.isLoaded ? edBtn : ""}
+                {this.state.isLoaded ? rmBtn : ""}
+            </ul>
         );
     }
 }
